7e6b9e609542626041c27e60b3d2b69a9746aee9
mravinale <mravinale@gmail.com>
2016-06-12 20:28:43 -0300

    improved detection

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-
 "use strict";
 
 var Bean = require('ble-bean');
-
 var mqtt = require('mqtt');
 
 var client = mqtt.connect({ 
@@ -15,56 +13,26 @@ var client = mqtt.connect({
 
 var intervalId;
 var connectedBean;
+var detectionFlag = false;
 var lastx = null;
 var lasty = null;
 var lastz = null;
-var offset = 0.09;
+var offset = 0.06;
 
-Bean.discover(function(bean){
-  connectedBean = bean;
-  process.on('SIGINT', exitHandler.bind(this));
+var stopInterval = function () {
+    clearInterval(intervalId);
+    connectedBean.setColor(new Buffer([0x0,0x0,0x0]), function(){});
+}
 
-  bean.on("accell", function(x, y, z, valid){
-    var status = valid ? "valid" : "invalid";
-  	
-    if(!lastx){ lastx = x; lasty = y; lastz = z; return; }
-   
-    var diffx = Math.abs(lastx) - Math.abs(x);
-    var diffy = Math.abs(lasty) - Math.abs(y);
-    var diffz = Math.abs(lastz) - Math.abs(z);
+var startInterval = function () {
+    intervalId = setInterval(readData,1000);
+}
 
-    if( Math.abs(diffx) > offset || Math.abs(diffy) > offset || Math.abs(diffz) > offset) {
-      console.log("received " + status + " accell\tx:\t" + x + "\ty:\t" + y + "\tz:\t" + z );
-      client.publish('/device/switch/key/1qaz2wsx', "{\"value\":\"1\",\"tag\":\"movement\"}");
-      client.publish('/device/switch/key/1qaz2wsx', "{\"value\":\"0\",\"tag\":\"movement\"}");
-    }
+var readData = function() {
 
-    lastx = x; 
-    lasty = y; 
-    lastz = z; 
-   
-  });
+if(detectionFlag) return; 
 
-  bean.on("temp", function(temp, valid){
-    var status = valid ? "valid" : "invalid";
-    console.log("received " + status + " temp:\t" + temp);
-  });
-
-  bean.on("disconnect", function(){
-    process.exit();
-  });
-
-  bean.connectAndSetup(function(){
-
-    var readData = function() {
-
-      //set random led colors between 0-255. I find red overpowering so red between 0-64
-/*   bean.setColor(new Buffer([getRandomInt(0,64),getRandomInt(0,255),getRandomInt(0,255)]),
-        function(){
-          console.log("led color sent");
-      });
-*/
-      bean.requestAccell(
+      connectedBean.requestAccell(
       function(){
         console.log("request accell sent");
       });
@@ -74,16 +42,64 @@ Bean.discover(function(bean){
         console.log("request temp sent");
       });
 */
+    
+};
+
+Bean.discover(function(bean){
+  connectedBean = bean;
+  process.on('SIGINT', exitHandler.bind(this));
+
+  connectedBean.on("accell", function(x, y, z, valid){
+    var status = valid ? "valid" : "invalid";
+    
+    if(detectionFlag) return; 
+  	
+    if(!lastx){ lastx = x; lasty = y; lastz = z; return; }
+   
+    var diffx = Math.abs(lastx) - Math.abs(x);
+    var diffy = Math.abs(lasty) - Math.abs(y);
+    var diffz = Math.abs(lastz) - Math.abs(z);
+
+    if( Math.abs(diffx) > offset || Math.abs(diffy) > offset || Math.abs(diffz) > offset) {
+      detectionFlag = true;
+      bean.setColor(new Buffer([255,0,0]), function(){
+          console.log("received " + status + " accell\tx:\t" + x + "\ty:\t" + y + "\tz:\t" + z );
+      });
+
+      client.publish('/device/switch/key/1qaz2wsx', "{\"value\":\"1\",\"tag\":\"movement\"}");
+      stopInterval();
+            
+      setTimeout(function(){
+        startInterval();
+        client.publish('/device/switch/key/1qaz2wsx', "{\"value\":\"0\",\"tag\":\"movement\"}"); 
+        detectionFlag = false;      
+      },5000); 
+            
     }
 
-    intervalId = setInterval(readData,1000);
+    lastx = x; 
+    lasty = y; 
+    lastz = z; 
+   
   });
+
+  connectedBean.on("temp", function(temp, valid){
+    var status = valid ? "valid" : "invalid";
+    console.log("received " + status + " temp:\t" + temp);
+  });
+
+  connectedBean.on("disconnect", function(){
+    process.exit();
+  });
+
+  connectedBean.connectAndSetup(function(){
+    startInterval();
+  });
+    
 
 });
 
-var getRandomInt = function(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+
 
 process.stdin.resume();//so the program will not close instantly
 var triedToExit = false;
@@ -95,9 +111,8 @@ var exitHandler = function exitHandler() {
   client.end();
   if (connectedBean && !triedToExit) {
     triedToExit = true;
-    console.log('Turning off led...');
-    clearInterval(intervalId);
-    connectedBean.setColor(new Buffer([0x0,0x0,0x0]), function(){});
+    stopInterval();
+  
     //no way to know if succesful but often behind other commands going out, so just wait 2 seconds
     console.log('Disconnecting from Device...');
     setTimeout(connectedBean.disconnect.bind(connectedBean, function(){}), 2000);
